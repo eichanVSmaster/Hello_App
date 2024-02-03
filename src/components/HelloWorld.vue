3313c3ff387db2704c029cb02fadc1fbbe005dd1
eichan <eichan216@gmail.com>
2024-02-04 01:15:12 +0900

    masterにプッシュ

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
     <button v-on:click="doAction">Show/Hide</button>
     <br />
     <transition name="transit">
-      <p v-if="flg" class="trans">ここを変更しました。</p>
+      <p v-if="flg" class="trans">今度はmasterにプッシュします。</p>
     </transition>
   </div>
 </template>
