0d5608833df95cc111d9eb8c03f97c69efe05f17
eichan <eichan216@gmail.com>
2024-02-03 18:03:57 +0900

    sourcetree test commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloWorld title="slot">
+      <li v-for="obj in slotobjs" v-bind:key="obj.name">{{ obj.name }}({{ obj.mail }})</li>
+    </HelloWorld>
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from "./components/HelloWorld.vue"
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+  },
+  data: function () {
+    return {
+      slotobjs: [
+        { name: "Taro", mail: "taro@yamada" },
+        { name: "Hanako", mail: "hanako@flower" },
+        { name: "Sachiko", mail: "sachiko@happy" },
+      ],
+    }
+  },
+}
+</script>
